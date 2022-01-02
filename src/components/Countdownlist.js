2a95a733ffdd3373fd30e0ad9e7ea379d66e7ef5
farhanreizha <reizha226@gmail.com>
2022-01-02 17:11:44 +0700

    first commit

--- src/components/Countdownlist.js
+++ src/components/Countdownlist.js
@@ -1,13 +1,14 @@
-import React, { useState, useEffect } from "react";
-import "../App.css";
-import Hope from "./Hope";
+import React, { useState, useEffect } from 'react';
+import '../App.css';
+import Hope from './Hope';
 
-function Countdownlist({ setCountdown, countdown }) {
-  const [time, setTime] = useState("");
-  const [newYear, setNewYear] = useState("New Year");
-  const [comingSoon, setComingSoon] = useState("Coming Soon");
+function Countdownlist({ setCountdown }) {
+  const [time, setTime] = useState('');
+  const [newYear, setNewYear] = useState('New Year');
+  const [comingSoon, setComingSoon] = useState('Coming Soon');
+
   useEffect(() => {
-    let countDownDate = new Date("Jan 1, 2022 00:00:00").getTime();
+    let countDownDate = new Date('Jan 1, 2022 00:00:00').getTime();
     // update every second
     let x = setInterval(function () {
       // Get today's date and time
@@ -23,14 +24,14 @@ function Countdownlist({ setCountdown, countdown }) {
       let minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
       let second = Math.floor((distance % (1000 * 60)) / 1000);
 
-      setTime(days + "d " + hour + "h " + minutes + "m " + second + "s ");
+      setTime(days + 'd ' + hour + 'h ' + minutes + 'm ' + second + 's ');
 
       if (distance < 0) {
         clearInterval(x);
-        setTime(<Hope/>);
+        setTime(<Hope />);
         setCountdown(true);
-        setNewYear("");
-        setComingSoon("");
+        setNewYear('');
+        setComingSoon('');
         setTimeout(() => {
           setCountdown(false);
         }, 15000);
@@ -38,9 +39,9 @@ function Countdownlist({ setCountdown, countdown }) {
     }, 1000);
   }, [setCountdown]);
   return (
-    <div className="content">
-      <div className="title">{newYear}</div>
-      <div className="subTitle">{comingSoon}</div>
+    <div className='content'>
+      <div className='title'>{newYear}</div>
+      <div className='subTitle'>{comingSoon}</div>
       <div>{time}</div>
     </div>
   );
